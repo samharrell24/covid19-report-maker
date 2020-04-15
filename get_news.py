998e991ac420ae05406d470685ed7e669bea9faf
sch9980 <sch9980@rit.edu>
2020-04-14 20:00:51 -0400

    Completed get_news.py

--- get_news.py
+++ get_news.py
@@ -8,10 +8,11 @@ class site:
     source: str
     author: str
     title: str
+    description: str
     url: str
     date_published: str
     content: str
-# , cnn, cnbc, new-york-times, fox-news, the-washington-post
+
 
 def get_article_data():
     client = NewsApiClient(api_key='32ac179029f9427c8c7a61f40e48c7c7')
@@ -20,23 +21,43 @@ def get_article_data():
                                              sources='bbc-news, cnn, cnbc, new-york-times, fox-news')
 
     # top_headlines is a dict that contains ['status', 'totalResults', 'articles']
-    print(top_headlines['totalResults'])
-    create_pretty_list(top_headlines['articles'], top_headlines['totalResults'])
+    aList = create_pretty_list(top_headlines['articles'], top_headlines['totalResults'])
+    return aList
 
 
 def create_pretty_list(dict, size):
     aList = []
 
     for i in range(0, size):
-        if i == size-1:
-            exit()
-        else:
-            for key, value in dict[i].items():
-                print(f"\n{key.ljust(15)} {value}")
+        temp_site = site("", "", "", "", "", "", "")
+
+        for key, value in dict[i].items():
+            if key == "source":
+                temp_site.source = value
+            elif key == "author":
+                temp_site.author = value
+            elif key == "title":
+                temp_site.title = value
+            elif key == "description":
+                temp_site.description = value
+            elif key == "url":
+                temp_site.url = value
+            elif key == "publishedAt":
+                temp = value.split("T")
+                temp2 = temp[1].split("Z")
+                temp_site.date_published = temp[0] + " at " + temp2[0]
+            elif key == "content":
+                temp_site.content = value
+
+        aList.append(temp_site)
+
+    return aList
 
 
 def main():
-    get_article_data()
+    aList = get_article_data()
+    for x in aList:
+        print(x)
 
 
 if __name__ == '__main__':
